0c04bffc8bbfda8366b8194a1319354363d130dd
johnnyboldt <johnny.boldt@gmail.com>
2018-08-30 10:48:01 -0600

    backend initial commit

---
+++ ItemAPI/ItemAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ItemAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ItemAPI/ItemAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ItemAPI.WebApiApplication" Language="C#" %>
